02f38efb8eeb43887871449d6402f57fb82d3a03
jesusparfer27 <jesusparfer@alumnos.cei.es>
2025-03-04 20:58:06 +0100

    commit

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename); 
 
-// Configura middlewares para CORS, JSON y datos codificados en URL.
+// Configura middlewares para CORS, JSON y datos codificados en URL. 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
